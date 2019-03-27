d2d146b41d5f2c31aa61842a0396c899a015c087
Ying Jin <yingjin.us@gmail.com>
2019-03-27 09:37:18 -0400

    Creating model in Shared project

---
+++ BlazorWithFirestore.Shared/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Google.Cloud.Firestore;
+
+namespace BlazorWithFirestore.Shared.Models
+{
+    /// <summary>
+    /// Decorate class with [FirestoreData] will allow mapping this object to Firestore collection.
+    /// Only properties marked with [FirestoreProperty] are considered when saving doc to collection
+    /// </summary>
+    [FirestoreData]
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public DateTime date { get; set; }
+        [FirestoreProperty]
+        public string EmployeeName { get; set; }
+        [FirestoreProperty]
+        public string CityName { get; set; }
+        [FirestoreProperty]
+        public string Designation { get; set; }
+        [FirestoreProperty]
+        public string Gender { get; set; }
+    }
+}
